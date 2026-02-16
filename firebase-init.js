89241d018de912b42ea6557707cdf5b594fd58a8
gingerjournal <linpeijiang6@gmail.com>
2026-02-16 00:48:14 -0500

    test

--- firebase-init.js
+++ firebase-init.js
@@ -5,7 +5,10 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 export const firebaseConfig = {
     apiKey: "AIzaSyAcFbugHulO4Jf7hkrSqolQRG_gQiGXc6A",
     authDomain: "gingerjournal-6e4ab.firebaseapp.com",
-    projectId: "ingerjournal-6e4ab",
+    projectId: "gingerjournal-6e4ab",
+    storageBucket: "gingerjournal-6e4ab.appspot.com",
+    messagingSenderId: "103389693685",
+    appId: "1:103389693685:web:8f4a4b8b8c1a6c9f2b1e3c"
 };
 
 export const app = initializeApp(firebaseConfig);
