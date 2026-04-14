4fc1d9484d05661f59fa0a772878652f4b3d86b9
gingerjournal <linpeijiang6@gmail.com>
2026-04-14 07:47:41 -0400

    changed buy me a coffee link to the left side of the screen for all pags.

--- GlobalFiles/HEADER_FOOTER.js
+++ GlobalFiles/HEADER_FOOTER.js
@@ -49,7 +49,7 @@ export function initHeaderMenu() {
 
 /* -----------------------------
    POPUP
-
+------------------------------*/
 export function initScrollPopup() {
     const HOURS = 1;
     const COOLDOWN = HOURS * 60 * 60 * 1000;
@@ -83,5 +83,3 @@ export function initScrollPopup() {
 
     window.addEventListener("scroll", showScrollPopup, { once: true });
 }
-
-------------------------------*/
